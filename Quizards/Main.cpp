2c13e01e6a09d557e713bcfab495c8ef9ce1fc52
JaysonC <jaysonc678@gmail.com>
2026-04-01 15:54:25 -0600

    Better wording on testing prints

--- Quizards/Main.cpp
+++ Quizards/Main.cpp
@@ -8,8 +8,8 @@ int main() {
 	
 	FlashCard randomCard = manager.randomCard();
 
-	std::cout << randomCard.getDef() << std::endl;
-	std::cout << randomCard.isCorrect("City") << std::endl;
-	std::cout << randomCard.isCorrect("Village") << std::endl;
+	std::cout << "The definition of the card is: " << randomCard.getDef() << std::endl;
+	std::cout << "Is the term city? " << randomCard.isCorrect("City") << std::endl;
+	std::cout << "Is the term village? " << randomCard.isCorrect("Village") << std::endl;
 
 }
