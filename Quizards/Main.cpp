4c7e4c585967d55f3d3401a6a5921e74254783e9
Jesse Wolford <jccw18@nmsu.edu>
2026-04-02 13:24:00 -0600

    Code I've written - Jesse

--- Quizards/Main.cpp
+++ Quizards/Main.cpp
@@ -9,7 +9,26 @@ int main() {
 	FlashCard randomCard = manager.randomCard();
 
 	std::cout << "The definition of the card is: " << randomCard.getDef() << std::endl;
+
+	/*
+	* This function will ask the user to input the answer from the question asked.
+	* @param answerInput will be the answer given by the user
+	* @return If answer is city after @param randomCard.isCorrect(""), it will be 1 or 0 otherwise.
+	*/
+	
+	std::string answerInput;
+	std::cout << "Answer: ";
+	std::cin >> answerInput;
+
+	if (randomCard.isCorrect(answerInput)) {
+		std::cout << "Great job!" << std::endl;
+	}
+	else {
+		std::cout << "Please try again!" << std::endl;
+	}
+	
 	std::cout << "Is the term city? " << randomCard.isCorrect("City") << std::endl;
 	std::cout << "Is the term village? " << randomCard.isCorrect("Village") << std::endl;
+
 
 }
